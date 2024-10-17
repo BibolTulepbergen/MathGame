daef5b3475ca5bc41ba58bb9b7ac1d058282cead
BibolTulepbergen <biboltulepbergen@gmail.com>
2024-10-17 22:36:44 +0500

    Добавил меню, в меню есть кнопка 'Играть' и 'Рекорды'.

--- MathGame/MainWindow.xaml.cs
+++ MathGame/MainWindow.xaml.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace MathGame
 {
-    using System.Windows.Threading;
     public partial class MainWindow : Window
     {
         DispatcherTimer timer = new DispatcherTimer();
         int tenthsOfSecondsElapsed;
         int matchesFound;
+        private int bestTime = int.MaxValue;
+
         public MainWindow()
         {
             InitializeComponent();
-
             timer.Interval = TimeSpan.FromSeconds(.1);
             timer.Tick += Timer_Tick;
-            SetUpGame();
-            timer.Start();
-            tenthsOfSecondsElapsed = 0;
-            matchesFound = 0;
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -31,10 +28,18 @@ namespace MathGame
             if (matchesFound == 8)
             {
                 timer.Stop();
-                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+                if (tenthsOfSecondsElapsed < bestTime)
+                {
+                    bestTime = tenthsOfSecondsElapsed;
+                    timeTextBlock.Text += " - New record!";
+                }
+                else
+                {
+                    timeTextBlock.Text += " - Play again?";
+                }
+                RecordTextBlock.Text = $"Лучший результат: {bestTime / 10.0:F1} секунд";
             }
         }
-
 
         private void SetUpGame()
         {
@@ -46,7 +51,6 @@ namespace MathGame
 
             Random random = new Random();
 
-            // Обходим все TextBlock в Grid
             foreach (TextBlock textBlock in MainGrid.Children.OfType<TextBlock>())
             {
                 if (textBlock.Name != "timeTextBlock")
@@ -58,10 +62,15 @@ namespace MathGame
                     animalEmoji.RemoveAt(index);
                 }
             }
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
         }
 
         TextBlock lastTextBlockClicked;
         bool findingMatch = false;
+
         private void TextBlock_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             TextBlock textBlock = sender as TextBlock;
@@ -91,5 +100,22 @@ namespace MathGame
                 SetUpGame();
             }
         }
+
+        // Обработка нажатия кнопки "Играть"
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuPanel.Visibility = Visibility.Collapsed;
+            MainGrid.Visibility = Visibility.Visible;
+            SetUpGame();
+        }
+
+        // Обработка нажатия кнопки "Рекорды"
+        private void RecordsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuPanel.Visibility = Visibility.Collapsed;
+            RecordsPanel.Visibility = Visibility.Visible;
+            RecordTextBlock.Text = $"Лучший результат: {bestTime / 10.0:F1} секунд";
+        }
     }
 }
+
